1feb205449171a9fd0f56700f0c306a897a7aa88
Robert Melendez <robertm3lendez@gmail.com>
2025-03-29 16:49:35 -0400

    hero page

---
+++ app/main/page.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import Image from "next/image";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+
+import { companyInfo } from "@/typing";
+import { Authenticated, Unauthenticated } from "convex/react";
+import Link from "next/link";
+import { ContactForm } from "@/components/ContactForm";
+import { ModeToggle } from "@/components/ModeToggle";
+
+export default function Home() {
+  return (
+    <div className="min-h-svh flex flex-col max-w-7xl mx-auto">
+      <header className="sticky top-0 z-10 bg-green-600 p-4 border-b-2 border-green-700 flex flex-row justify-between items-center text-white">
+        <div className="flex items-center space-x-2">
+          <Image
+            src="/logo.png"
+            alt="Breidys Logo"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <span className="text-xl font-bold">Breidys Tree Services</span>
+        </div>
+        <div className="flex items-center gap-5 ml-auto pr-2">
+          <Unauthenticated>
+            <ModeToggle className="dark:bg-primary bg-green-600" />
+            <SignInButton />
+          </Unauthenticated>
+        </div>
+        <div className="flex items-center gap-5">
+          <Authenticated>
+            <Link href={"/protected"} className="font-medium">
+              Admin
+            </Link>
+            <UserButton />
+          </Authenticated>
+        </div>
+      </header>
+      <main className="p-8 flex flex-col gap-8 justify-between">
+        <section className="text-center">
+          <h1 className="text-5xl font-bold text-green-700">
+            Welcome to Breidys Tree Services
+          </h1>
+          <p className="mt-4 text-lg text-gray-700">
+            Your trusted partner in landscaping, tree care, and removal.
+          </p>
+        </section>
+        <h2 className="text-4xl font-bold text-green-700 text-center">
+          Services
+        </h2>
+        <section className="flex flex-col md:flex-row gap-8">
+          <div className="flex-1">
+            <Image
+              src="/tree3.png"
+              alt="Landscaping"
+              width={600}
+              height={400}
+              className="rounded-lg shadow-lg"
+            />
+            <h2 className="text-2xl font-semibold mt-4 text-green-700">
+              Landscaping
+            </h2>
+            <p className="text-gray-700">
+              Enhance the beauty and health of your outdoor space with our
+              expert tree care and landscaping services. Let us create a
+              thriving, green environment for you to enjoy!
+            </p>
+          </div>
+          <div className="flex-1">
+            <Image
+              src="/tree2.png"
+              alt="Tree Care"
+              width={600}
+              height={400}
+              className="rounded-lg shadow-lg"
+            />
+            <h2 className="text-2xl font-semibold mt-4 text-green-700">
+              Tree Care
+            </h2>
+            <p className="text-gray-700">
+              Protect and enhance the natural beauty of your landscape with our
+              professional tree care services. From expert pruning and
+              maintenance to disease prevention and removal, we ensure your
+              trees stay healthy, vibrant, and safe for years to come
+            </p>
+          </div>
+          <div className="flex-1">
+            <Image
+              src="/tree1.png"
+              alt="Tree Removal"
+              width={600}
+              height={400}
+              className="rounded-lg shadow-lg"
+            />
+            <h2 className="text-2xl font-semibold mt-4 text-green-700">
+              Tree Removal
+            </h2>
+            <p className="text-gray-700">
+              Whether you’re dealing with a hazardous tree, storm damage, or
+              simply making space for a new project, our skilled team provides
+              safe, efficient, and professional tree removal services tailored
+              to your needs
+            </p>
+          </div>
+        </section>
+        <section className="text-center mt-8">
+          <h2 className="text-4xl font-bold text-green-700">Contact Us</h2>
+          <p className="mt-4 text-lg text-gray-700">
+            At {companyInfo.name}, we are committed to preserving the beauty and
+            health of your trees with expert care and sustainable solutions.
+            Whether you need pruning, removal, or routine maintenance, our team
+            is here to help. Contact us today and let’s nurture your outdoor
+            space together!
+          </p>
+          <div className="mt-8 mb-8">
+            <ContactForm />
+          </div>
+          <Button variant="outline" className="mt-4">
+            Get a Quote
+          </Button>
+        </section>
+      </main>
+      <footer className="bg-green-600 p-4 text-center text-white">
+        <p>
+          &copy; {new Date().getFullYear()} Breidys Tree Services. All rights
+          reserved.
+        </p>
+      </footer>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,134 +1,75 @@
 "use client";
 
-import Image from "next/image";
-import { SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
-import { companyInfo } from "@/typing";
-import { Authenticated, Unauthenticated } from "convex/react";
-import Link from "next/link";
-import { ContactForm } from "@/components/ContactForm";
-import { ModeToggle } from "@/components/ModeToggle";
-
-export default function Home() {
+export default function ExplorePage() {
+  const router = useRouter();
   return (
-    <div className="min-h-svh flex flex-col max-w-7xl mx-auto">
-      <header className="sticky top-0 z-10 bg-green-600 p-4 border-b-2 border-green-700 flex flex-row justify-between items-center text-white">
-        <div className="flex items-center space-x-2">
-          <Image
-            src="/logo.png"
-            alt="Breidys Logo"
-            width={40}
-            height={40}
-            className="rounded-full"
-          />
-          <span className="text-xl font-bold">Breidys Tree Services</span>
-        </div>
-        <div className="flex items-center gap-5 ml-auto pr-2">
-          <Unauthenticated>
-            <ModeToggle className="dark:bg-primary bg-green-600" />
-            <SignInButton />
-          </Unauthenticated>
-        </div>
-        <div className="flex items-center gap-5">
-          <Authenticated>
-            <Link href={"/protected"} className="font-medium">
-              Admin
-            </Link>
-            <UserButton />
-          </Authenticated>
-        </div>
-      </header>
-      <main className="p-8 flex flex-col gap-8 justify-between">
-        <section className="text-center">
-          <h1 className="text-5xl font-bold text-green-700">
-            Welcome to Breidys Tree Services
-          </h1>
-          <p className="mt-4 text-lg text-gray-700">
-            Your trusted partner in landscaping, tree care, and removal.
-          </p>
-        </section>
-        <h2 className="text-4xl font-bold text-green-700 text-center">
-          Services
-        </h2>
-        <section className="flex flex-col md:flex-row gap-8">
-          <div className="flex-1">
-            <Image
-              src="/tree3.png"
-              alt="Landscaping"
-              width={600}
-              height={400}
-              className="rounded-lg shadow-lg"
-            />
-            <h2 className="text-2xl font-semibold mt-4 text-green-700">
-              Landscaping
-            </h2>
-            <p className="text-gray-700">
+    <div className="min-h-screen">
+      <div className="relative min-h-svh flex items-center">
+        <Image
+          src="/tree1.png"
+          alt="Tree Service Hero"
+          fill
+          className="object-cover brightness-70"
+          priority
+        />
+        <div className="absolute inset-0 bg-black/30" />
+        <div className="relative z-10 container mx-auto px-4">
+          <motion.div
+            className="max-w-2xl"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <motion.h1
+              className="text-5xl font-bold text-white mb-4"
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.3 }}
+            >
+              BREIDYS' TREE CARE,
+            </motion.h1>
+            <motion.h2
+              className="text-3xl font-light text-white mb-6"
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.6 }}
+            >
+              Nurturing Nature.
+            </motion.h2>
+            <motion.p
+              className="text-lg text-gray-200 mb-8"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.7 }}
+            >
               Enhance the beauty and health of your outdoor space with our
               expert tree care and landscaping services. Let us create a
               thriving, green environment for you to enjoy!
-            </p>
-          </div>
-          <div className="flex-1">
-            <Image
-              src="/tree2.png"
-              alt="Tree Care"
-              width={600}
-              height={400}
-              className="rounded-lg shadow-lg"
-            />
-            <h2 className="text-2xl font-semibold mt-4 text-green-700">
-              Tree Care
-            </h2>
-            <p className="text-gray-700">
-              Protect and enhance the natural beauty of your landscape with our
-              professional tree care services. From expert pruning and
-              maintenance to disease prevention and removal, we ensure your
-              trees stay healthy, vibrant, and safe for years to come
-            </p>
-          </div>
-          <div className="flex-1">
-            <Image
-              src="/tree1.png"
-              alt="Tree Removal"
-              width={600}
-              height={400}
-              className="rounded-lg shadow-lg"
-            />
-            <h2 className="text-2xl font-semibold mt-4 text-green-700">
-              Tree Removal
-            </h2>
-            <p className="text-gray-700">
-              Whether you’re dealing with a hazardous tree, storm damage, or
-              simply making space for a new project, our skilled team provides
-              safe, efficient, and professional tree removal services tailored
-              to your needs
-            </p>
-          </div>
-        </section>
-        <section className="text-center mt-8">
-          <h2 className="text-4xl font-bold text-green-700">Contact Us</h2>
-          <p className="mt-4 text-lg text-gray-700">
-            At {companyInfo.name}, we are committed to preserving the beauty and
-            health of your trees with expert care and sustainable solutions.
-            Whether you need pruning, removal, or routine maintenance, our team
-            is here to help. Contact us today and let’s nurture your outdoor
-            space together!
-          </p>
-          <div className="mt-8 mb-8">
-            <ContactForm />
-          </div>
-          <Button variant="outline" className="mt-4">
-            Get a Quote
-          </Button>
-        </section>
-      </main>
-      <footer className="bg-green-600 p-4 text-center text-white">
-        <p>
-          &copy; {new Date().getFullYear()} Breidys Tree Services. All rights
-          reserved.
-        </p>
-      </footer>
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.9 }}
+            >
+              <Button
+                size="lg"
+                variant="secondary"
+                className="bg-white text-gray-900 hover:bg-gray-100 hover:scale-105 transition-transform"
+                onClick={() => {
+                  router.push("/main");
+                }}
+              >
+                Explore more
+              </Button>
+            </motion.div>
+          </motion.div>
+        </div>
+      </div>
     </div>
   );
 }
